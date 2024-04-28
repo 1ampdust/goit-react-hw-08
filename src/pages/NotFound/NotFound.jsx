6133e6535ef3cce0515b06f89bdcb7d9d0d942ba
Lampdust <lampdust06@gmail.com>
2024-04-29 01:23:34 +0300

    Create NotFound.jsx

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+
+  return (
+    <div>
+      <h1>Page you visited doesn&apos;t exist.</h1>
+      <Link to="/">Go Home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
